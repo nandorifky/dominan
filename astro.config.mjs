65050eb2781766c961bc7a33b8eb771ba9b0585f
Nando Rifky <mr.winando@gmail.com>
2026-01-17 04:28:50 +0700

    sa

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import markdoc from '@astrojs/markdoc';
-import cloudflare from '@astrojs/cloudflare'; // [1] Import ini
+import cloudflare from '@astrojs/cloudflare'; // [1] Import 
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://dominan.pages.dev',
   output: 'static', // Bisa tetap static, adapter akan handle fallback
   adapter: cloudflare(), // [2] Pasang adapter ini
   integrations: [
