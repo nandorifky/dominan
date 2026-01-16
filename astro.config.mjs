aa756135fb4fd79482a8c5c500579ae9b79d2ab1
Nando Rifky <mr.winando@gmail.com>
2026-01-17 05:03:54 +0700

    sa

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap'; // [Added by CLI]
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://dominan.pages.dev', // [WAJIB DIISI] Ganti dengan domain asli Anda
+  site: 'https://dominan.web.id', // [WAJIB DIISI] Ganti dengan domain asli Anda
   output: 'static',
   adapter: cloudflare(),
   integrations: [
