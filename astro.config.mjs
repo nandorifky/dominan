90c86838331e162ca50c4aedc053cefb937469a8
Nando Rifky <mr.winando@gmail.com>
2026-01-17 00:14:41 +0700

    Upload pertama: Website & Blog JSON Ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
-import markdoc from '@astrojs/markdoc'; // Pastikan ini ada
+import markdoc from '@astrojs/markdoc';
+import cloudflare from '@astrojs/cloudflare'; // [1] Import ini
 
 export default defineConfig({
-  output: 'static',
+  output: 'static', // Bisa tetap static, adapter akan handle fallback
+  adapter: cloudflare(), // [2] Pasang adapter ini
   integrations: [
     tailwind(),
     react(),
     keystatic(),
-    markdoc(), // Pastikan ini ada di daftar
+    markdoc(),
   ],
 });
